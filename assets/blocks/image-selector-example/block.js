57faf56b38233d97f1c956f58bdc820ff273ea30
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-06-28 13:09:36 +0200

    remove unused comments

--- assets/blocks/image-selector-example/block.js
+++ assets/blocks/image-selector-example/block.js
@@ -11,10 +11,9 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
 const { InnerBlocks } = wp.editor;
 
 registerBlockType( 'image-selector-example/image-selector', {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
 	title: __( 'Image Selector Example', 'image-selector-example' ), // Block title.
-	icon: 'format-image', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	icon: 'format-image',
+	category: 'common',
 	keywords: [
 		__( 'Image Selector', 'image-selector-example' ),
 	],
